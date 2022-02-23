c6278117b1868352a085b9a1949231f2e15d67d8
leo0807 <zhangjunxu3@gmail.com>
2022-02-23 13:17:54 +0800

    feat: 🎸 update auth modal

--- packages/docs/.examples/f-auth-method-modal/basic.vue
+++ packages/docs/.examples/f-auth-method-modal/basic.vue
@@ -1,26 +1,67 @@
 <template>
-  <f-auth-method-modal fennec title="Connect Wallet" @auth="handleAuth">
+  <f-auth-method-modal
+    ref="authModal"
+    :fennec="fennec"
+    :client-id="clientId"
+    :code-challenge="codeChallenge"
+    :scope="scope"
+    :state="state"
+    :is-firesbox="isFiresbox"
+    title="Connect Wallet"
+    @auth="handleAuth"
+    @error="handleError"
+  >
     <template #activator="{ on }">
       <v-btn depressed rounded color="primary" v-on="on"> Connect </v-btn>
     </template>
   </f-auth-method-modal>
 </template>
 <script>
+import axios from "axios";
 export default {
-  data() {
+  data () {
     return {
-      show: false
-    };
+      fennec: false,
+      isFiresbox: false,
+      clientId: "75f18fe8-b056-46d6-9c48-0214425e58ce",
+      scope: "PROFILE:READ ASSETS:READ",
+      codeChallenge: "",
+      state: ""
+    }
   },
   methods: {
-    handleShowAuthModal() {
-      this.show = true;
+
+    async handleAuth (value) {
+      if (value.authCode) {
+        await this.authMixin(value.authCode);
+      }
+      else if (value.fennec) {
+        console.log(value.fennec);
+      }
     },
 
-    handleAuth(val) {
-      console.log(val);
+    handleError (error) {
+      console.log(error)
+    },
+
+    async authMixin(code) {
+
+      const url = "https://leaf-api.pando.im/api/login";
+      fetch(url, {
+        method: "POST",
+        body:JSON.stringify({code: code})
+      }).then(res =>
+        res.json().then(data => ({
+          data:data,
+          status: res.status
+        }))
+      ).then(res => {
+        const token = res.data.data.token;
+        console.log(token);
+      });
+
     }
   }
-};
+}
 </script>
 <style lang="scss" scoped></style>
